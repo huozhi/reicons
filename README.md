197cc8aa69d4f9eb74cd156b17d058c9796f1665
huozhi <gilesliu12@gmail.com>
2016-11-22 17:29:07 +0800

    tidy up, remove css import, use embeded style instead

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
-# Reicons
+Reicons
+===
 
 See more details in `demo` directory or visit website: [huozhi.github.io/reicons/](https://huozhi.github.io/reicons/)
 
 
-Generate SVG icons with React, control SVG attributes by compositing props.
+Generate SVG icons with React, control SVG attributes by compositing props. Pure javascript with a little bit embedded css style.
+
+![screenshot](https://raw.githubusercontent.com/huozhi/huozhi.github.io/hexo/source/img/2016/respinner/static-icons.jpg)
+
+
+## Usage
+
+```js
+import {Cross} from 'reicons'
+
+<Cross size={32} color={color} strokeWidth={3} rotate={0} />
+```
--- src/components/Add.js
+++ src/components/Add.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import './Add.css'
 
 const Add = ({size, color, thickness, dash}) => {
+  const halfWidth = size / 2
   const barHeight = size / 3
   const borderRadius = thickness / 2
   const dasharray = dash instanceof Array ? dash.join() : dash
@@ -19,18 +19,23 @@ const Add = ({size, color, thickness, dash}) => {
 
   return (
     <svg className="Add" width={size} height={size}>
+      <style type="text/css" dangerouslySetInnerHTML={{__html: `
+        .Add-plus {
+          transform-origin: center;
+        }
+      `}} />
       <circle
         fill="none"
         className="Add-border"
         strokeDasharray={dasharray}
-        cx={size / 2}
-        cy={size / 2}
+        cx={halfWidth}
+        cy={halfWidth}
         r={(size - thickness / 2) / 2}
         strokeWidth={thickness / 2}
         stroke={color}
       />
       <g>
-        <rect className="Add-plus" {...barProps} />
+        <rect transform={`rotate(90, ${halfWidth}, ${halfWidth})`} className="Add-plus" {...barProps} />
         <rect className="Add-plus" {...barProps} />
       </g>
     </svg>
